6f3dd2b0bce863e8bd0c2e2442a24078646f3f8f
Muhammad Tayyab <309957@twgroup.co.nz>
2022-01-29 12:37:16 +1300

    added wrapper

--- nodejs_dsl.groovy
+++ nodejs_dsl.groovy
@@ -2,6 +2,9 @@ job('nodejs-example-dsl') {
     scm {
        github('wardviaene/node-demo-app', 'master')
     }
+    wrappers {
+        nodejs('nodejs')
+    }
     steps {
         shell('npm install')
     }
