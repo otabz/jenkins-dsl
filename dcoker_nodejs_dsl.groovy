8314e2b27243239efd49122a776224c219febb60
Muhammad Tayyab <309957@twgroup.co.nz>
2022-01-29 13:32:37 +1300

    docker dsl

---
+++ dcoker_nodejs_dsl.groovy
@@ -0,0 +1,15 @@
+job('docker-nodejs-example-dsl') {
+    scm {
+       github('wardviaene/docker-demo', 'master')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('otabz/nodejs-example')
+            tag('${BUILD_TIMESTAMP}-${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
